62e25a999045b30ae8d529b0322d9a6b5f973502
Dan Wanek <dan.wanek@gmail.com>
2010-07-14 10:15:31 -0500

    removed legacy exchwebserv. new base methods added.

--- lib/exchwebserv.rb
+++
@@ -1,6 +0,0 @@
-# This exists for legacy reasons and will be depreciated soon
-warn "*******************************************"
-warn "Requiring exchwebserv is depreciated and will be removed soon.\nPlease require 'viewpoint'"
-warn "*******************************************"
-
-require 'viewpoint'
--- lib/viewpoint.rb
+++ lib/viewpoint.rb
@@ -1,5 +1,57 @@
-module Viewpoint
-end
+#############################################################################
+# Copyright © 2010 Dan Wanek <dan.wanek@gmail.com>
+#
+#
+# This file is part of Viewpoint.
+# 
+# Viewpoint is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+# 
+# Viewpoint is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with Viewpoint.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+require 'singleton'
 
-require 'viewpoint/errors'
-require 'viewpoint/exchwebserv'
+# Load the backend SOAP infrastructure.  Today this is Handsoap.
+#require 'soap/soap_provider'
+
+# Load the model classes
+#require 'model/sp_message'
+
+
+module Viewpoint
+  module EWS
+    class EWS
+      include Singleton
+
+      attr_reader :ews
+
+      def self.endpoint=(endpoint)
+        @@endpoint = endpoint
+      end
+
+      def self.endpoint
+        @@endpoint
+      end
+
+      def self.set_auth(user,pass)
+        @@user = user
+        @@pass = pass
+      end
+
+      def initialize
+        require 'soap/soap_provider'
+
+        @ews = SOAP::ExchangeWebService.new
+      end
+
+    end # class EWS
+  end # EWS
+end
