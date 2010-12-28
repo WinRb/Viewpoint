72d552056936098f5912515c43dd1139c489f257
Dan Wanek <dan.wanek@gmail.com>
2010-12-27 22:14:58 -0600

    add ability to pass a shape to Item::get_item

--- lib/model/item.rb
+++ lib/model/item.rb
@@ -31,11 +31,12 @@ module Viewpoint
       # This is a class method that fetches an existing Item from the
       #  Exchange Store.
       # @param [String] item_id The id of the item.
+      # @param [Symbol] shape The optional shape of the item :id_only/:default/:all_properties
       # @return [Item] Returns an Item or subclass of Item
       # @todo Add support to fetch an item with a ChangeKey
-      def self.get_item(item_id)
-        conn = Viewpoint::EWS::EWS.instance
-        resp = conn.ews.get_item([item_id])
+      def self.get_item(item_id, shape = :default)
+        item_shape = {:base_shape => shape.to_s.camelcase}
+        resp = (Viewpoint::EWS::EWS.instance).ews.get_item([item_id], item_shape)
         resp = resp.items.shift
         resp_type = resp.keys.first
         eval "#{resp_type.to_s.camel_case}.new(resp[resp_type])"
