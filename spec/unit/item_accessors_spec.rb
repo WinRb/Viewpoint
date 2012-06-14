eea0316f966a7dd1e8ec858e44e2718de277b7cd
Dan Wanek <dan.wanek@gmail.com>
2012-06-14 14:00:29 -0500

    ItemAccessors unit tests

---
+++ spec/unit/item_accessors_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../spec_helper'
+
+module ResponseObjects
+  def self.folders
+    [ {:folder => {:display_name => {:text => 'Inbox'}}},
+      {:folder => {:display_name => {:text => 'Drafts'}}},
+      {:tasks_folder => {:display_name => {:text => 'Tasks'}}} ]
+  end
+end
+
+describe Viewpoint::EWS::ItemAccessors do
+  before do
+    ews = double("ews")
+    @ecli = double("EWSClient")
+    @ecli.extend subject
+    @ecli.stub(:ews) {ews}
+  end
+
+  context "ensure that exceptions are being raised" do
+    before do
+      resp = OpenStruct.new
+      resp.status = 'Failure'
+      @ecli.ews.stub(:get_item).with(an_instance_of(Hash)) { resp }
+    end
+    it '#get_item should raise an exception' do
+      expect {
+        @ecli.get_item("MyItemId")
+      }.to raise_error(Viewpoint::EWS::EwsError)
+    end
+  end
+
+end
