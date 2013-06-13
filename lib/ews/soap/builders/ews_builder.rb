e67a387ea6b1f86b2ad0710afad41da252262e82
Roman Messer <rms@gotocloud.net>
2013-06-13 16:45:39 +0200

    Added namespace to SetItemField in EWS builder.

--- lib/ews/soap/builders/ews_builder.rb
+++ lib/ews/soap/builders/ews_builder.rb
@@ -857,9 +857,9 @@ module Viewpoint::EWS::SOAP
       uri = upd.select {|k,v| k =~ /_uri/i}
       raise EwsBadArgumentError, "Bad argument given for SetItemField." if uri.keys.length != 1
       upd.delete(uri.keys.first)
-      @nbuild.SetItemField {
-        dispatch_field_uri!(uri)
-        dispatch_field_item!(upd)
+      @nbuild[NS_EWS_TYPES].SetItemField {
+        dispatch_field_uri!(uri, NS_EWS_TYPES)
+        dispatch_field_item!(upd, NS_EWS_TYPES)
       }
     end
 
