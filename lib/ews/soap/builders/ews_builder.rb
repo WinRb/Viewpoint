31d121dd68af831d78498e1d61645f069f0ce4d9
mapimopi <mapimopipi@gmail.com>
2013-07-04 08:04:29 +0400

    main impersonation functionality

--- lib/ews/soap/builders/ews_builder.rb
+++ lib/ews/soap/builders/ews_builder.rb
@@ -48,6 +48,7 @@ module Viewpoint::EWS::SOAP
         node.parent.namespace = parent_namespace(node)
         node.Header {
           set_version_header! opts[:server_version]
+		      set_impersonation! opts[:impersonation_type], opts[:impersonation_mail]
           yield(:header, self) if block_given?
         }
         node.Body {
@@ -1034,6 +1035,16 @@ private
         }
       end
     end
+
+    def set_impersonation!(type, address)
+	    if type && type != ""
+	      nbuild[NS_EWS_TYPES].ExchangeImpersonation {
+		      nbuild[NS_EWS_TYPES].ConnectingSID {
+		        nbuild[NS_EWS_TYPES].method_missing type, address
+		      }
+        }
+      end
+	  end
 
     # some methods need special naming so they use the '_r' suffix like 'and'
     def normalize_type(type)
