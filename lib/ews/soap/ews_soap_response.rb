f59f58150718506501ad4ce72d7a8bbd761250cf
Lleïr Borràs Metje <lleir@llegeix.me>
2013-07-19 10:57:46 +0200

    Change the rubygems source for the https version, include rake gem on development to work with rbenv

--- lib/ews/soap/ews_soap_response.rb
+++ lib/ews/soap/ews_soap_response.rb
@@ -45,7 +45,11 @@ module Viewpoint::EWS::SOAP
 
     def response_messages
       key = response.keys.first
-      response[key][:elems][0][:response_messages][:elems]
+      if elem = response[key][:elems].find{|e| e.has_key?(:response_message)}
+       elem[:response_messages][:elems]
+      else
+        []
+      end
     end
 
     def response_message
