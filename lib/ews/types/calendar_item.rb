5b731ca75293484bbd522d9e1dae5131c899bcc5
Roman Messer <rms@gotocloud.net>
2013-10-01 18:25:42 +0200

    Fixed CalendarItem#update_item! when unknown attributes are passed.

--- lib/ews/types/calendar_item.rb
+++ lib/ews/types/calendar_item.rb
@@ -25,20 +25,21 @@ module Viewpoint::EWS::Types
     # @option options :conflict_resolution [String] one of 'NeverOverwrite', 'AutoResolve' (default) or 'AlwaysOverwrite'
     # @option options :send_meeting_invitations_or_cancellations [String] one of 'SendToNone' (default), 'SendOnlyToAll',
     #   'SendOnlyToChanged', 'SendToAllAndSaveCopy' or 'SendToChangedAndSaveCopy'
-    # @return [CalendarItem]
+    # @return [CalendarItem, false]
     # @example Update Subject and Body
     #   item = #...
     #   item.update_item!(subject: 'New subject', body: 'New Body')
     # @see http://msdn.microsoft.com/en-us/library/exchange/aa580254.aspx
     # @todo AppendToItemField updates not implemented
     def update_item!(updates, options = {})
-      item_updates = updates.map do |attribute, value|
+      item_updates = []
+      updates.each do |attribute, value|
         item_field = FIELD_URIS[attribute][:text] if FIELD_URIS.include? attribute
         field = {field_uRI: {field_uRI: item_field}}
 
-        if value.nil?
-          {delete_item_field: field}
-        else
+        if value.nil? && item_field
+          item_updates << {delete_item_field: field}
+        elsif item_field
           item = Viewpoint::EWS::Template::CalendarItem.new(attribute => value)
 
           # Remap attributes because ews_builder #dispatch_field_item! uses #build_xml!
@@ -57,20 +58,28 @@ module Viewpoint::EWS::Types
             end
           end
 
-          {set_item_field: field.merge(calendar_item: {sub_elements: item_attributes})}
+          item_updates << {set_item_field: field.merge(calendar_item: {sub_elements: item_attributes})}
+        else
+          # Ignore unknown attribute
         end
       end
 
-      data = {}
-      data[:conflict_resolution] = options[:conflict_resolution] || 'AutoResolve'
-      data[:send_meeting_invitations_or_cancellations] = options[:send_meeting_invitations_or_cancellations] || 'SendToNone'
-      data[:item_changes] = [{item_id: self.item_id, updates: item_updates}]
-      rm = ews.update_item(data).response_messages.first
-      if rm && rm.success?
-        self.get_all_properties!
-        self
-      else
-        raise EwsCreateItemError, "Could not update calendar item. #{rm.code}: #{rm.message_text}" unless rm
+      if item_updates.any?
+        data = {}
+        data[:conflict_resolution] = options[:conflict_resolution] || 'AutoResolve'
+        data[:send_meeting_invitations_or_cancellations] = options[:send_meeting_invitations_or_cancellations] || 'SendToNone'
+        data[:item_changes] = [{item_id: self.item_id, updates: item_updates}]
+
+        warn "Builder Hash"
+        warn data
+
+        rm = ews.update_item(data).response_messages.first
+        if rm && rm.success?
+          self.get_all_properties!
+          self
+        else
+          raise EwsCreateItemError, "Could not update calendar item. #{rm.code}: #{rm.message_text}" unless rm
+        end
       end
 
     end
