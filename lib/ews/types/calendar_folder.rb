f4eacf915e2514513c26ab2392cab73a8d530132
Dan Wanek <dan@9summer.com>
2013-10-10 16:35:26 -0500

    Fixes #100: move calendar_items_between to calendar_folder * rename to #items_between to keep same signature

--- lib/ews/types/calendar_folder.rb
+++ lib/ews/types/calendar_folder.rb
@@ -4,5 +4,29 @@ module Viewpoint::EWS::Types
     include Viewpoint::EWS::Types
     include Viewpoint::EWS::Types::GenericFolder
 
+    # Fetch items between a given time period
+    # @param [DateTime] start_date the time to start fetching Items from
+    # @param [DateTime] end_date the time to stop fetching Items from
+    def items_between(start_date, end_date, opts={})
+      items do |obj|
+        obj.restriction = { :and =>
+          [
+            {:is_greater_than_or_equal_to =>
+              [
+                {:field_uRI => {:field_uRI=>'calendar:Start'}},
+                {:field_uRI_or_constant=>{:constant => {:value =>start_date}}}
+              ]
+            },
+            {:is_less_than_or_equal_to =>
+              [
+                {:field_uRI => {:field_uRI=>'calendar:End'}},
+                {:field_uRI_or_constant=>{:constant => {:value =>end_date}}}
+              ]
+            }
+          ]
+        }
+      end
+    end
+
   end
 end
--- lib/ews/types/generic_folder.rb
+++ lib/ews/types/generic_folder.rb
@@ -104,30 +104,6 @@ module Viewpoint::EWS::Types
       end
     end
 
-    # Fetch items between a given time period
-    # @param [DateTime] start_date the time to start fetching Items from
-    # @param [DateTime] end_date the time to stop fetching Items from
-    def calendar_items_between(start_date, end_date, opts={})
-      items do |obj|
-        obj.restriction = { :and =>
-          [
-            {:is_greater_than_or_equal_to =>
-              [
-                {:field_uRI => {:field_uRI=>'calendar:Start'}},
-                {:field_uRI_or_constant=>{:constant => {:value =>start_date}}}
-              ]
-            },
-            {:is_less_than_or_equal_to =>
-              [
-                {:field_uRI => {:field_uRI=>'calendar:End'}},
-                {:field_uRI_or_constant=>{:constant => {:value =>end_date}}}
-              ]
-            }
-          ]
-        }
-      end
-    end
-
     # Search on the item subject
     # @param [String] match_str A simple string paramater to match against the
     #   subject.  The search ignores case and does not accept regexes... only strings.
