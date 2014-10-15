1ad9ae2354ffb204870b8596832eb3b339e52367
mr-deamon <christian.lehmann@swisstxt.ch>
2014-10-15 15:25:40 +0200

    Update connection.rb
    Added ssl_version to allow connections to servers with TLS-only

--- lib/ews/connection.rb
+++ lib/ews/connection.rb
@@ -39,6 +39,7 @@ class Viewpoint::EWS::Connection
       end
     end
     @httpcli.ssl_config.verify_mode = opts[:ssl_verify_mode] if opts[:ssl_verify_mode]
+    @httpcli.ssl_config.ssl_version = opts[:ssl_version] if opts[:ssl_version]
     # Up the keep-alive so we don't have to do the NTLM dance as often.
     @httpcli.keep_alive_timeout = 60
     @httpcli.receive_timeout = opts[:receive_timeout] if opts[:receive_timeout]
