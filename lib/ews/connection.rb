2bac8b07c1df5c40b9b5861696661c30b49396e2
Wes Hather <wes@grouptalent.com>
2015-03-24 17:26:16 -0700

    Add in ability to override receive_timeout and send_timeout

--- lib/ews/connection.rb
+++ lib/ews/connection.rb
@@ -27,6 +27,8 @@ class Viewpoint::EWS::Connection
   # @param [Hash] opts Misc config options (mostly for developement)
   # @option opts [Fixnum] :ssl_verify_mode
   # @option opts [Array]  :trust_ca an array of hashed dir paths or a file
+  # @option opts [Fixnum] :receive_timeout override the default receive timeout
+  # @option opts [Fixnum] :send_timeout override the default send timeout
   def initialize(endpoint, opts = {})
     @log = Logging.logger[self.class.name.to_s.to_sym]
     @httpcli = HTTPClient.new
@@ -39,6 +41,8 @@ class Viewpoint::EWS::Connection
     @httpcli.ssl_config.verify_mode = opts[:ssl_verify_mode] if opts[:ssl_verify_mode]
     # Up the keep-alive so we don't have to do the NTLM dance as often.
     @httpcli.keep_alive_timeout = 60
+    @httpcli.receive_timeout = opts[:receive_timeout] if opts[:receive_timeout]
+    @httpcli.send_timeout = opts[:send_timeout] if opts[:send_timeout]
     @endpoint = endpoint
   end
 
