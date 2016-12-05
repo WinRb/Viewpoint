d1f2c9215d11fab944158e1fab5c3fae8d5efaec
Garry Shutler <garry@robustsoftware.co.uk>
2016-12-05 16:44:17 +0000

    Strip more known bad characters before parsing
    Removes all the non-printable characters excluded from the XML
    [characters specification][1].
    
    [1]: https://www.w3.org/TR/xml/#charsets

--- lib/ews/soap/parsers/ews_parser.rb
+++ lib/ews/soap/parsers/ews_parser.rb
@@ -28,7 +28,7 @@ module Viewpoint::EWS::SOAP
 
     def parse(opts = {})
       opts[:response_class] ||= EwsSoapResponse
-      @soap_resp.gsub!(/&#x1A;/, '')
+      @soap_resp.gsub!(/&#x([0-8bcef]|1[0-9a-f]);/i, '')
       sax_parser.parse(@soap_resp)
       opts[:response_class].new @sax_doc.struct
     end
