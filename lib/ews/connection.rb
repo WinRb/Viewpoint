d91cc3229da193d1a290594867632304f577f742
Bank Lee <iambanklee@gmail.com>
2019-03-20 11:47:21 +0000

    [SM-429] Error message enhancement

--- lib/ews/connection.rb
+++ lib/ews/connection.rb
@@ -203,7 +203,7 @@ class Viewpoint::EWS::Connection
     when 401
       raise Errors::UnauthorizedResponseError.new("Unauthorized request", resp)
     when 429
-      raise Errors::TooManyRequestsError.new("TooManyRequests", resp)
+      raise Errors::TooManyRequestsError.new("Too many requests", resp)
     when 500
       if resp.headers['Content-Type'] =~ /xml/
         err_string, err_code = parse_soap_error(resp.body)
